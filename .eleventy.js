88072aefd12922618417a2b5841063ee63244dfa
SquirrelKiev <laidbackenon@outlook.com>
2022-09-27 23:33:03 +0100

    use scss magic, add twemoji support, general backend stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,20 @@
 const eleventySass = require("eleventy-sass");
 const eleventyFeathericons = require('eleventy-plugin-feathericons');
+const twemoji = require("twemoji")
 
-module.exports = eleventyConfig => 
-{
+module.exports = eleventyConfig => {
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPlugin(eleventyFeathericons);
 
   eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.addTransform("parse-twemoji", function (content, outputPath) {
+    if (outputPath.endsWith(".css") === false) {
+      content = twemoji.parse(content);
+    }
+
+    return content;
+  });
 
   // 11ty defaults
   return {
